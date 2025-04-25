d28899eae042e222c4cb7121d775aa644166c976
wiseman-umanah <wisemanumanah@gmail.com>
2025-04-26 00:25:33 +0100

    Redeployed token on Lisk Sepolia Network

--- intro_to_hardhat/hardhat.config.ts
+++ intro_to_hardhat/hardhat.config.ts
@@ -14,9 +14,30 @@ const config: HardhatUserConfig = {
       url: `${SEPOLIA_RPC}`,
       accounts: SEPOLIA_PRIVATE_KEY ? [SEPOLIA_PRIVATE_KEY] : [],
     },
+	'lisk-sepolia': {
+      url: 'https://rpc.sepolia-api.lisk.com',
+      accounts: [SEPOLIA_PRIVATE_KEY],
+      gasPrice: 1000000000,
+    },
   },
   etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
+    // Use "123" as a placeholder, because Blockscout doesn't need a real API key, and Hardhat will complain if this property isn't set.
+    apiKey: {
+      "lisk-sepolia": "123"
+    },
+    customChains: [
+      {
+          network: "lisk-sepolia",
+          chainId: 4202,
+          urls: {
+              apiURL: "https://sepolia-blockscout.lisk.com/api",
+              browserURL: "https://sepolia-blockscout.lisk.com"
+          }
+      }
+    ]
+  },
+  sourcify: {
+    enabled: false
   },
 };
 
